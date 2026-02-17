e1ac67b585957065281fd554b7be141b47a4d46d
Parv Chauhan <parvchauhan112@gmail.com>
2026-02-17 15:03:57 +0530

    intial commint - Dockerized node app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express =require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req ,res )=> {
+	res.send ('DevOps Projecty is running successfully');
+});
+app.listen(port,() => {
+	console.log(`server running on port ${port}`);
+});
